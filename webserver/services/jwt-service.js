ea81a9603acfd39a171bd1ab44dacd25c247fdb1
Unknown <gonzalozasreg@gmail.com>
2019-04-08 17:03:20 +0200

    Sustitucion de callback por await/async en funcion de validacion de token

--- webserver/services/jwt-service.js
+++ webserver/services/jwt-service.js
@@ -4,28 +4,21 @@ const jwt = require('jsonwebtoken');
 const config = require('../../config/http-server-config');
 
 let validate = async (req, res, next) => {
-  const token = req.get('JWTtoken'); // nombre a poner en postman
+    const token = req.get('JWTtoken'); // nombre a poner en postman
 
-  if (!token) {
-    return res.status(401).send();
-  }
+    if (!token) {
+        return res.status(401).json({ message: 'No token sent' });
+    }
 
-  try {
-
-    jwt.verify(token, config.tokenSeed, (err, decoded) => {
-      
-      if (err) {
-        return res.status(401).send();
-      }
-
-      req.user = decoded.user; 
-      next();
-    });
-
-  } catch (e) {
-    return res.status(401).send();
-  }
+    try {
+        let decoded = await jwt.verify(token, config.tokenSeed);
+        req.user = decoded.user;
+        next();
+    } catch (e) {
+        let msg = e.message || e;
+        return res.status(401).json({ message: msg });
+    }
 }
 
 
-module.exports = {validate};
+module.exports = { validate };
