d9a502455a19faa82ace487d73792be937b132ab
Unknown <gonzalozasreg@gmail.com>
2019-04-15 23:18:00 +0200

    Corregido error de asyncronia en subida de imagenes a Cloudinary

--- webserver/services/file-service.js
+++ webserver/services/file-service.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const cloudinary = require('cloudinary').v2;
+const path = require('path');
 
 const cloudName = process.env.CLOUDINARI_CLOUD_NAME;
 const apiKey = process.env.CLOUDINARI_API_KEY;
@@ -12,47 +13,25 @@ cloudinary.config({
   api_secret: apiSecret,
 });
 
-const upload = async (id, data) => {
+const upload = async (id, file) => {
 
-    let url = '';
+    try {
 
-    console.log('path :', data);
+        let tempPath = path.join('./tmp', file.name)
+        await file.mv(tempPath)
 
-    // await cloudinary.uploader.upload(new Buffer(data).toString('base64'), {
-    //   resource_type: 'raw',
-    //   public_id: id,
-    //   width: 200,
-    //   height: 200,
-    //   format: 'jpg',
-    //   crop: 'limit'
-    // })
-    // .then(result => {
-  
-    //   console.log('result :', result);
-    //   url = result.secure_url;
-    // })
-    // .catch(err => {
-    //     throw err;
-    // });
-
-    await cloudinary.uploader.upload_stream(
-      {
-        resource_type: "raw",
-        public_id: id,
-        width: 200,
-        height: 200,
-        format: "jpg",
-        crop: "limit",
-        async: true
-      },
-      async (err, result) => {
-          if (err) throw err;
-
-          url = result.secure_url;
-      }).end(data);
-    
-    return url;
+        return new Promise((resolve, reject) => {
+            cloudinary.uploader.upload(tempPath, (error, result) => {
+                if (error) 
+                    reject(error);
+                else
+                    resolve(result.secure_url);
+            });
+        });
+    } catch (err) {
+        throw err;
+    }
 }
 
-module.exports =  { upload };
+module.exports = { upload };
 
