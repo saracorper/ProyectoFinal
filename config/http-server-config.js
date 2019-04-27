8cc735481a6cd0aef427807f98734f6143928b0d
Sara <cordalpernassl@gmail.com>
2019-04-27 14:04:04 +0200

    cambio de duracion del token

--- config/http-server-config.js
+++ config/http-server-config.js
@@ -4,7 +4,7 @@ const config = {
     port: process.env.PORT || 3000,
     mongoDbUrl: 'mongodb://heroku_fm4rqtg4:rinkn951e2rlt6qmhrlkm53qkr@ds231956.mlab.com:31956/heroku_fm4rqtg4',
     tokenSeed: 'dev-seed',
-    tokenExpireTime: '1h',
+    tokenExpireTime: '1d',
     frontDomain: 'http://localhost:4200'
 };
 
